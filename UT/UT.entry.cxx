e5ef2c91171bb6ce51e25e2c0deb77990c4ad7a8
Mike Nitro <wcdnail@gmail.com>
2024-04-28 22:15:18 +0300

    ++ UT

---
+++ UT/UT.entry.cxx
@@ -0,0 +1,16 @@
+#include "pch.hxx"
+#include <gtest/gtest.h>
+
+#pragma comment(lib, "atls.lib")
+
+#if !defined(_CONSOLE)
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, PTSTR lpCmdLine, int nShowCmd)
+#endif
+int _tmain(int argc, TCHAR* argv[])
+{
+    SetErrorMode(SetErrorMode(0) | SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX);
+    DefWindowProc(NULL, 0, 0, 0L);
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ UT/pch.cxx
@@ -0,0 +1,5 @@
+#include "pch.hxx"
+
+// DO NOT put any code here!! 
+// It's a pre-compiled header cache.
+
