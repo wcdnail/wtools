ef25b1098e0e282878660c140433056bfc50adb7
Mike Nitro <wcdnail@gmail.com>
2024-05-05 14:46:16 +0300

    +-+- exa1

---
+++ exa/wtl.photothing/stdafx.h
@@ -0,0 +1,20 @@
+#include <sdkddkver.h>
+
+//#define WINVER            0x0500
+//#define _WIN32_WINNT      0x0501
+//#define _WIN32_IE         0x0600
+#define _RICHEDIT_VER     0x0700
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <atlbase.h>
+#include <atlapp.h>
+
+extern WTL::CAppModule _Module;
+
+#include <atlwin.h>
+#include <atlmisc.h>
+#include <atlctrls.h>
+#include <atltheme.h>
+
+#include <shlwapi.h>
+
