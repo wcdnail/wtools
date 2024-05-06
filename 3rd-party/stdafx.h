2584736a7b7616b6b9fb46c08e5180c7cb386c16
Mike Nitro <wcdnail@gmail.com>
2024-05-06 17:54:15 +0300

    ++ TATOR initial

---
+++ 3rd-party/stdafx.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "targetver.h"
+
+#define STRICT
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+#include <atlbase.h>
+#include <atlwin.h>
+#include <atlstr.h>
+#include <atlapp.h>
+#include <atldlgs.h>
+#include <atlframe.h>
+#include <atlctrls.h>
+#include <atlctrlx.h>
+#include <atlimage.h>
+
+#define UNREFERENCED_ARG(a) (a)
+
+#undef max
+#undef min
---
+++ tator/stdafx.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+#pragma comment(lib, "atls.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "shlwapi.lib")
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "msimg32.lib")
