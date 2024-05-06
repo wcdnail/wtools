9754c2aac9aff6a8106aa734598cb335b40bbd26
Mike Nitro <wcdnail@gmail.com>
2024-05-06 21:55:20 +0300

    ++ 2 triangle grad, fignya

--- common/WTL/CColorPicker.h
+++ common/WTL/CColorPicker.h
@@ -24,10 +24,12 @@ struct CColorPicker: CColorPickerBase
     BEGIN_MSG_MAP_EX(CColorPicker)
         MSG_WM_CREATE(OnCreate)
         MSG_WM_ERASEBKGND(OnEraseBkgnd)
+        MSG_WM_PAINT(OnPaint)
     END_MSG_MAP()
 
     int OnCreate(LPCREATESTRUCT pCS);
     BOOL OnEraseBkgnd(CDCHandle dc) const;
+    void OnPaint(CDCHandle);
 };
 
 __declspec(selectany) ATOM CColorPicker::gs_Atom{0};
@@ -73,10 +75,56 @@ inline CColorPicker& CColorPicker::operator=(HWND hWnd)
 
 inline BOOL CColorPicker::OnEraseBkgnd(CDCHandle dc) const
 {
-    CRect rc;
+    //CRect rc;
+    //GetClientRect(rc);
+    //dc.FillSolidRect(rc, m_clrBkgnd);
+    return TRUE;
+}
+
+inline void CColorPicker::OnPaint(CDCHandle)
+{
+    CPaintDC paintDC{m_hWnd};
+    CDCHandle     dc{paintDC};
+    CRect         rc{};
+    COLORREF    clLT{0x00ffffff};
+    COLORREF    clRT{0x00fc2500};
+    COLORREF    clRB{0x00000000};
+    COLORREF    clLB{0x00000000};
+
     GetClientRect(rc);
     dc.FillSolidRect(rc, m_clrBkgnd);
-    return TRUE;
+
+    TRIVERTEX vertex[4] = 
+    {
+        { rc.left, rc.top,
+            static_cast<COLOR16>(static_cast<USHORT>(GetRValue(clLT)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetGValue(clLT)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetBValue(clLT)) << 8),
+            0x0000
+        },
+        { rc.right, rc.top,
+            static_cast<COLOR16>(static_cast<USHORT>(GetRValue(clRT)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetGValue(clRT)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetBValue(clRT)) << 8),
+            0x0000
+        },
+        { rc.right, rc.bottom,
+            static_cast<COLOR16>(static_cast<USHORT>(GetRValue(clRB)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetGValue(clRB)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetBValue(clRB)) << 8),
+            0x0000
+        },
+        { rc.left, rc.bottom,
+            static_cast<COLOR16>(static_cast<USHORT>(GetRValue(clLB)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetGValue(clLB)) << 8),
+            static_cast<COLOR16>(static_cast<USHORT>(GetBValue(clLB)) << 8),
+            0x0000
+        },
+    };
+
+    GRADIENT_TRIANGLE triGrad[2] = {{1, 2, 3},{0, 1, 3}};
+    GradientFill(dc, vertex, _countof(vertex), triGrad, _countof(triGrad), GRADIENT_FILL_TRIANGLE);
+
 }
 
 inline int CColorPicker::OnCreate(LPCREATESTRUCT pCS)
