ec466d14bb3b0a1b27e2c3fe60b98639c7a3765a
Sergey Bodrov <wcdnail@gmail.com>
2024-05-19 13:41:59 +0300

    +=

--- common/DDraw.DGI/DDGDIStuff.cpp
+++ common/DDraw.DGI/DDGDIStuff.cpp
@@ -14,17 +14,22 @@
 
 constexpr int IntMult = 20;
 
-static void RGB_Line(DWORD* pDest, int nWidth, int vStart, int iR, int iG, int iB)
+static void RGB_Line3(DWORD* pDest, int nWidth, int bR, int bG, int bB, int iR, int iG, int iB)
 {
-    int vR{vStart};
-    int vG{vStart};
-    int vB{vStart};
+    int vR{bR};
+    int vG{bG};
+    int vB{bB};
     for (int j = 0; j < nWidth; j++) {
         *pDest++ = RGB(vR >> IntMult, vG >> IntMult, vB >> IntMult);
         vR += iR;
         vG += iG;
         vB += iB;
     }
+}
+
+static void RGB_Line1(DWORD* pDest, int nWidth, int bRGB, int iR, int iG, int iB)
+{
+    RGB_Line3(pDest, nWidth, bRGB, bRGB, bRGB, iR, iG, iB);
 }
 
 bool DDraw_HSV_SAT_Line(DWORD* pDest, int nWidth, double dHue, double dValue)
@@ -52,12 +57,12 @@ bool DDraw_HSV_SAT_Line(DWORD* pDest, int nWidth, double dHue, double dValue)
     const int     inc2{static_cast<int>((1 - dFrac) * vStart - vStart) / nWidth};
     const int     inc3{static_cast<int>(dFrac * vStart - vStart) / nWidth};
     switch (nInt) {
-    case 0: RGB_Line(pDest, nWidth, vStart, inc1, inc3, 0); break;
-    case 1: RGB_Line(pDest, nWidth, vStart, inc1, 0, inc2); break;
-    case 2: RGB_Line(pDest, nWidth, vStart, inc3, 0, inc1); break;
-    case 3: RGB_Line(pDest, nWidth, vStart, 0, inc2, inc1); break;
-    case 4: RGB_Line(pDest, nWidth, vStart, 0, inc1, inc3); break;
-    case 5: RGB_Line(pDest, nWidth, vStart, inc2, inc1, 0); break;
+    case 0: RGB_Line1(pDest, nWidth, vStart, inc1, inc3, 0); break;
+    case 1: RGB_Line1(pDest, nWidth, vStart, inc1, 0, inc2); break;
+    case 2: RGB_Line1(pDest, nWidth, vStart, inc3, 0, inc1); break;
+    case 3: RGB_Line1(pDest, nWidth, vStart, 0, inc2, inc1); break;
+    case 4: RGB_Line1(pDest, nWidth, vStart, 0, inc1, inc3); break;
+    case 5: RGB_Line1(pDest, nWidth, vStart, inc2, inc1, 0); break;
     default:
         // TODO: report nInt - INVAL
         return false;
@@ -94,71 +99,53 @@ bool DDraw_HSV_Hue(CDibEx const& dibDest, double dHue)
         dHue);
 }
 
-void DDraw_HSV_HUE_Line(DWORD *buffer, int samples, double sat, double val_fp)
+void DDraw_HSV_HUE_Line(DWORD* pDest, int nWidth, double dSaturation, double dVal)
 {
-    // value, but as integer in [0, 255 << IntMult]
-    int     val;
-
-    // loop counter
-    int     j;
-
-    // coefficients and advances
-    int     coef1, coef2, coef3;
-    int     coef2_adv, coef3_adv;
-
-    // current position and advance to the next one
-    double  pos, pos_adv;
-
     //
     // hue increments in [0, 360); indirectly
     //  intp changes - 0, 1, 2, 3, 4, 5; indirectly (separate loops)
     //  frac increments in [0, 1) six times; indirectly (coefficients)
-    // sat - const, in [0, 1]
+    // dSaturation - const, in [0, 1]
     // val - const, in [0, (255 << IntMult)]
     //
-    // coef1 => val * (1 - sat)              => const, = val * (1 - sat)
-    // coef2 => val * (1 - sat * frac)       => changes from val to val * (1 - sat)
-    // coef3 => val * (1 - sat * (1 - frac)) => changes from val * (1 - sat) to val
+    // coef1 => val * (1 - dSaturation)              => const, = val * (1 - dSaturation)
+    // coef2 => val * (1 - dSaturation * frac)       => changes from val to val * (1 - dSaturation)
+    // coef3 => val * (1 - dSaturation * (1 - frac)) => changes from val * (1 - dSaturation) to val
     //
 
-    // constants
-    val = (int) (val_fp * 255) << IntMult;
-    coef1 = (int) (val * (1 - sat));
-
-    // prepare
-    pos = 0;
-    pos_adv = (double) samples / 6.0;
+    // value, but as integer in [0, 255 << IntMult]
+    int        val{(int)(dVal * 255.) << IntMult};
+    int      coef1{(int)(val * (1. - dSaturation))};
+    // current position and advance to the next one
+    double     pos{0};
+    double pos_adv{(double)nWidth / 6.0};
 
     // hue in [0, 60)
     pos += pos_adv;
-    j = (int) pos;
-    coef3 = coef1, coef3_adv = (int) ((val - coef3) / ((j) - 1));
-    while (j--) {
-        *buffer++ = RGB((BYTE) (coef1 >> int_extend),(BYTE) (coef3 >> int_extend),(BYTE) (val >> int_extend));
-        coef3 += coef3_adv;
-    }
+    int j = (int) pos;
+    int coef3 = coef1;
+    int coef3_adv = (int) ((val - coef3) / ((j) - 1));
+    RGB_Line3(pDest, j, coef1, coef3, val, 0, coef3_adv, 0);
 
     pos += pos_adv;
     j = (int) pos - (int) (1 * pos_adv);
-    coef2 = val, coef2_adv = (int) ((val * (1.0 - sat) - coef2) / ((j) - 1));
-    while (j--) {
-        *buffer++ = RGB ((BYTE) (coef1 >> int_extend),(BYTE) (val >> int_extend),(BYTE) (coef2 >> int_extend));
-        coef2 += coef2_adv;
-    }
+    int coef2 = val;
+    int coef2_adv = (int) ((val * (1.0 - dSaturation) - coef2) / ((j) - 1));
+    RGB_Line3(pDest, j, coef1, val, coef2, 0, 0, coef2_adv);
 
     pos += pos_adv;
     j = (int) pos - (int) (2 * pos_adv);
     coef3 = coef1, coef3_adv = (int) ((val - coef3) / ((j) - 1));
     while (j--) {
-        *buffer++ = RGB ((BYTE) (coef3 >> int_extend),(BYTE) (val >> int_extend),(BYTE) (coef1 >> int_extend));
+        *pDest++ = RGB ((coef3 >> int_extend),(val >> int_extend),(coef1 >> int_extend));
         coef3 += coef3_adv;
     }
 
     pos += pos_adv;
     j = (int) pos - (int) (3 * pos_adv);
-    coef2 = val, coef2_adv = (int) ((val * (1.0 - sat) - coef2) / ((j) - 1));
+    coef2 = val, coef2_adv = (int) ((val * (1.0 - dSaturation) - coef2) / ((j) - 1));
     while (j--) {
-        *buffer++ = RGB((BYTE) (val >> int_extend),(BYTE) (coef2 >> int_extend),(BYTE) (coef1 >> int_extend));
+        *pDest++ = RGB((val >> int_extend),(coef2 >> int_extend),(coef1 >> int_extend));
         coef2 += coef2_adv;
     }
 
@@ -166,15 +153,15 @@ void DDraw_HSV_HUE_Line(DWORD *buffer, int samples, double sat, double val_fp)
     j = (int) pos - (int) (4 * pos_adv);
     coef3 = coef1, coef3_adv = (int) ((val - coef3) / ((j) - 1));
     while (j--) {
-        *buffer++ = RGB ((BYTE) (val >> int_extend),(BYTE) (coef1 >> int_extend),(BYTE) (coef3 >> int_extend));
+        *pDest++ = RGB ((val >> int_extend),(coef1 >> int_extend),(coef3 >> int_extend));
         coef3 += coef3_adv;
     }
 
     pos += (pos_adv + 0.1); // + 0.1 because of floating-point math's rounding errors
     j = (int) pos - (int) (5 * pos_adv);
-    coef2 = val, coef2_adv = (int) ((val * (1.0 - sat) - coef2) / HSV_LOOP_STEPS (j));
+    coef2 = val, coef2_adv = (int) ((val * (1.0 - dSaturation) - coef2) / HSV_LOOP_STEPS (j));
     while (j--) {
-        *buffer++ = RGB ( (BYTE) (coef2 >> int_extend),(BYTE) (coef1 >> int_extend),(BYTE) (val >> int_extend));
+        *pDest++ = RGB ( (coef2 >> int_extend),(coef1 >> int_extend),(val >> int_extend));
         coef2 += coef2_adv;
     }
 }
